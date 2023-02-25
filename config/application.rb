af06bb41d5c38247a43e4cd207bec581cf2821b6
kolan4ick <Kolan4ick@gmail.com>
2023-02-25 21:18:51 +0200

    Change Timezone to "Europe/Kyiv"

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,10 @@ module AssistorAPI
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = 'Europe/Kyiv'
+    config.active_record.default_timezone = :local
+
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
