c05ef63d45013bf4a178922cad44fdfd5b34cdad
kolan4ick <Kolan4ick@gmail.com>
2023-02-28 01:01:32 +0200

    Fix sending email for registered volunteer

--- app/controllers/volunteers/registrations_controller.rb
+++ app/controllers/volunteers/registrations_controller.rb
@@ -22,16 +22,15 @@ class Volunteers::RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
-
-        # Send email to volunteer
-        VolunteerMailer.registration_email(resource)
-
         render json: resource
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         render json: resource
       end
+
+      # Send email to volunteer
+      VolunteerMailer.registration_email(resource)
     else
       clean_up_passwords resource
       set_minimum_password_length
