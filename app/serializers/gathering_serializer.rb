7a63ca89e162a3701510a09263a0e08ff1d366e7
kolan4ick <Kolan4ick@gmail.com>
2023-03-21 22:33:41 +0200

    Return is_favourite which indicates whether the gathering is a favorite or not

--- app/serializers/gathering_serializer.rb
+++ app/serializers/gathering_serializer.rb
@@ -3,7 +3,7 @@
 class GatheringSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   attributes :id, :title, :description, :sum, :start, :end, :ended, :verification, :link, :photos,
-             :finished_photos, :created_at, :updated_at, :creator_id, :gathering_category_id
+             :finished_photos, :created_at, :updated_at, :creator_id, :gathering_category_id, :is_favorite
 
   def photos
     object.photos.map { |photo| rails_blob_url(photo.blob, expires_in: 10.minutes, disposition: 'inline', signed: true) }
@@ -11,5 +11,11 @@ class GatheringSerializer < ActiveModel::Serializer
 
   def finished_photos
     object.finished_photos.map { |photo| rails_blob_url(photo.blob, expires_in: 10.minutes, disposition: 'inline', signed: true) }
+  end
+
+  def is_favorite
+    favouritable = current_user || current_volunteer
+
+    favouritable.favourites.include?(object)
   end
 end
