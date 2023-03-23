ff629fa5142d96966150d125d4e98007232d6734
kolan4ick <Kolan4ick@gmail.com>
2023-03-24 00:01:30 +0200

    Remove expiration time from images in serializers

--- app/serializers/gathering_serializer.rb
+++ app/serializers/gathering_serializer.rb
@@ -7,11 +7,11 @@ class GatheringSerializer < ActiveModel::Serializer
              :finished_photos, :created_at, :updated_at, :creator_id, :gathering_category_id, :is_favourite
 
   def photos
-    object.photos.map { |photo| rails_blob_url(photo.variant(resize_to_limit: [1170, 2532]), expires_in: 10.minutes, disposition: 'inline', signed: true) }
+    object.photos.map { |photo| rails_blob_url(photo.variant(resize_to_limit: [1170, 2532]), disposition: 'inline', signed: true) }
   end
 
   def finished_photos
-    object.finished_photos.map { |photo| rails_blob_url(photo.variant(resize_to_limit: [1170, 2532]), expires_in: 10.minutes, disposition: 'inline', signed: true) }
+    object.finished_photos.map { |photo| rails_blob_url(photo.variant(resize_to_limit: [1170, 2532]), disposition: 'inline', signed: true) }
   end
 
   def is_favourite
