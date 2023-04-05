a114c98f75ae2af27f28f5ae76af20287ca105c7
kolan4ick <Kolan4ick@gmail.com>
2023-04-05 10:47:28 +0300

    Add is_editable field to gathering_serializer for detect if gathering can be edited (also destroyed) by the volunteer

--- app/serializers/gathering_serializer.rb
+++ app/serializers/gathering_serializer.rb
@@ -4,7 +4,7 @@ class GatheringSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id, :title, :description, :sum, :start, :end, :ended, :verification, :link, :photos,
-             :finished_photos, :created_at, :updated_at, :creator_id, :gathering_category_id, :is_favourite
+             :finished_photos, :created_at, :updated_at, :creator_id, :gathering_category_id, :is_favourite, :is_editable
 
   def photos
     object.photos.map { |photo| rails_blob_url(photo.variant(resize_to_limit: [1170, 2532]), disposition: 'inline', signed: true) }
@@ -18,5 +18,9 @@ class GatheringSerializer < ActiveModel::Serializer
     favouritable = scope
 
     favouritable.favourites.include?(object)
+  end
+
+  def is_editable
+    object.creator == scope
   end
 end
