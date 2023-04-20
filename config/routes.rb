6074b86ca02a7712ec9cd7a14c78f4d625a61c16
kolan4ick <Kolan4ick@gmail.com>
2023-04-20 22:47:13 +0300

    Override reset_password_instructions mailer file and methods in passwords_controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   namespace :api do
     namespace :v1 do
-      devise_for :volunteers, controllers: { sessions: "volunteers/sessions", registrations: 'volunteers/registrations' }, singular: "volunteer"
-      devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }, singular: "user"
+      devise_for :volunteers, controllers: { sessions: "volunteers/sessions", registrations: 'volunteers/registrations',
+                                             passwords: "volunteers/passwords" }, singular: "volunteer"
+      devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations",
+                                        passwords: "users/passwords" }, singular: "user"
       resources :gathering_categories
       resources :gatherings do
         post :create_view, to: 'gatherings#create_view'
