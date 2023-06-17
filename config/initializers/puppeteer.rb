fa0741cb4b634bf8b4348e70be9f6dfd5a69dc17
Mykola Yuryshynets <Kolan4ick@gmail.com>
2023-06-18 00:20:34 +0300

    Uncomment puppeteer browser initialization in cofig

--- config/initializers/puppeteer.rb
+++ config/initializers/puppeteer.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
+frozen_string_literal: true
 
-# require 'puppeteer-ruby'
-#
-# # Create a global variable to hold the browser instance
-# $browser = Puppeteer.launch(headless: true)
+require 'puppeteer-ruby'
+
+# Create a global variable to hold the browser instance
+$browser = Puppeteer.launch(headless: true)
